e14de7ddc16bd3e545428839a2721fce4773e9ef
Salman-sajib <sfsajib@gmail.com>
2024-06-27 12:44:23 +0600

    added all the styles and functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,82 @@
-import { useState } from 'react';
-import Cell from './components/Cell';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import Cell from "./components/Cell";
 
 function App() {
-  const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
+  const [winningMessage, setWinningMessage] = useState(null);
+
+  const message = `It is now ${go}'s go.`;
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winningCombos.forEach((array) => {
+      let circleWins = array.every((cell) => cells[cell] === "circle");
+      if (circleWins) {
+        setWinningMessage("Circle Wins!");
+        return;
+      }
+    });
+
+    winningCombos.forEach((array) => {
+      let crossWins = array.every((cell) => cells[cell] === "cross");
+      if (crossWins) {
+        setWinningMessage("Cross Wins!");
+        return;
+      }
+    });
+  };
+
+  const handleReset = () => {
+    setCells(["", "", "", "", "", "", "", "", ""]);
+    setGo("circle");
+    setWinningMessage(null);
+  };
+
+  useEffect(() => {
+    checkScore();
+  }, [cells]);
 
   return (
-    <div className='app'>
-      <div className='gameboard'>
+    <div className="flex h-screen flex-col items-center justify-center bg-zinc-950 text-gray-100">
+      <h1 className="mb-[5rem] text-4xl font-bold text-gray-200">
+        Tic-Tac-Toe
+      </h1>
+      {/* gameboard */}
+      <div className="flex size-[350px] flex-wrap items-center justify-center gap-3">
         {cells.map((cell, index) => (
-          <Cell key={index} id={index} cell={cell} />
+          <Cell
+            key={index}
+            id={index}
+            cell={cell}
+            go={go}
+            setGo={setGo}
+            cells={cells}
+            setCells={setCells}
+            winningMessage={winningMessage}
+          />
         ))}
       </div>
+      <p className="mt-5 text-xl font-medium">{winningMessage || message}</p>
+      {winningMessage && (
+        <button
+          onClick={handleReset}
+          className="mt-3 rounded-md bg-orange-400 px-6 py-2 text-lg text-zinc-950 transition-colors hover:bg-orange-500"
+        >
+          Reset
+        </button>
+      )}
     </div>
   );
 }
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,8 +1,50 @@
 /* eslint-disable react/prop-types */
-const Cell = ({ id, cell }) => {
+const Cell = ({ id, cell, go, setGo, cells, setCells, winningMessage }) => {
+  const handleClick = (e) => {
+    // console.log(e.target);
+    if (!winningMessage) {
+      const taken =
+        e.target.firstChild?.classList.contains("circle") ||
+        e.target.firstChild?.classList.contains("cross") ||
+        e.target.classList.contains("circle") ||
+        e.target.classList.contains("cross");
+
+      if (!taken) {
+        if (go === "circle") {
+          e.target.firstChild.classList.add("circle");
+          handleCellChange("circle");
+          setGo("cross");
+        } else if (go === "cross") {
+          e.target.firstChild.classList.add("cross");
+          handleCellChange("cross");
+          setGo("circle");
+        }
+      }
+    }
+  };
+
+  const handleCellChange = (className) => {
+    const nextCells = cells.map((cell, index) => {
+      if (index === id) {
+        return className;
+      } else {
+        return cell;
+      }
+    });
+    setCells(nextCells);
+  };
+
+  // console.log(cells);
+
   return (
-    <div className='square' id={id}>
-      {cell}
+    <div className="flex size-[105px] items-center justify-center rounded-md bg-gradient-to-tr from-blue-300 via-green-300 to-pink-400">
+      <div
+        className="flex size-[99px] cursor-pointer items-center justify-center rounded-md bg-zinc-950"
+        id={id}
+        onClick={handleClick}
+      >
+        <div className={cell}></div>
+      </div>
     </div>
   );
 };
